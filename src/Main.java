521bd0de52305f104e25c45704c6a1df877b905f
Jerry Won <jwon1750@gmail.com>
2022-11-10 19:38:44 -0800

    Commit

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,21 @@ public class Main {
     }
 
     // take user input string
-    public static String takeInput() {
+    public static String takeInputString() {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        return input;
+        return scanner.nextLine();
+    }
+
+    //take user input int
+    public static int takeInputInt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 
     // ask for username input
     public static String askForUsername() {
         System.out.println("Please enter your username: ");
-        String input = takeInput();
-        return input;
+        return takeInputString();
     }
 
     // start the game
@@ -29,4 +33,20 @@ public class Main {
         System.out.println("Welcome " + player.username + "!");
     }
 
+    //add story to the game
+    public static void story(int chapter) {
+        if (chapter == 1) {
+            System.out.println("Story goes here");
+        } else if (chapter == 2) {
+            System.out.println("Story goes here");
+        } else if (chapter == 3) {
+            System.out.println("Story goes here");
+        } else if (chapter == 4) {
+            System.out.println("Story goes here");
+        } else if (chapter == 5) {
+            System.out.println("Story goes here");
+        } else if (chapter == 6) {
+            System.out.println("Story goes here");
+        }
+    }
 }

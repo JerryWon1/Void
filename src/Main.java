bb6f54eeaa71aff1aa1e41cabe3f57916ac3a42d
Jerry Won <jwon1750@gmail.com>
2022-11-11 20:25:33 -0800

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     //game start
@@ -11,33 +12,10 @@ public class Main {
         gameLoop();
     }
 
-    // take user input string
-    public static String takeInputString() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
-
-    //take user input int
-    public static int takeInputInt() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-
-    // ask for username input
-    public static String askForUsername() {
-        System.out.println("Please enter your username: ");
-        return takeInputString();
-    }
-
     // game loop
     public static void gameLoop() {
         gamePlay();
         while (gameRunning) {
-            if (takeInputString().equals("t")) {
-                gameMenuOpen = true;
-            } else {
-                System.out.println("That does nothing!");
-            }
             if (gameMenuOpen) {
                 gameMenu();
             } else {
@@ -53,31 +31,45 @@ public class Main {
     }
 
     // game menu
-    public static void gameMenu() {
-        System.out.println("What would you like to do?");
+    public static void gameMenu(String... pick) {
+        System.out.println("\nWhat would you like to do?");
         System.out.println("1. Add something to inventory");
         System.out.println("2. Remove something from inventory");
         System.out.println("3. Display inventory");
         System.out.println("4. Exit menu");
         System.out.println("5. Exit game");
 
-        int choice = takeInputInt();
+        char choice = takeInputChar();
 
-        if (choice == 1) {
+        if (choice == '1' || pick.equals("add")) {
             // add something to inventory
             System.out.println("What would you like to add?");
             String item = takeInputString();
             player.addItem(item);
-        } else if (choice == 2) {
+        } else if (choice == '2' || pick.equals("remove")) {
             //remove something from inventory
             System.out.println("What would you like to remove? (Type number) Inventory: " + player.inventory);
             String location = takeInputString();
             player.removeItem(location);
-        } else if (choice == 3) {
-            System.out.println(player.inventory);
-        } else if (choice == 4) {
+        } else if (choice == '3' || pick.equals("inventory")) {
+            boolean back = false;
+            System.out.println("Inventory: ");
+            while (!back) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(player.inventory);
+                System.out.println("Press b to go back");
+                if (takeInputChar() == 'b') {
+                    back = true;
+                }
+            }
+
+        } else if (choice == '4' || pick.equals("exit menu")) {
             gameMenuOpen = false;
-        } else if (choice == 5) {
+        } else if (choice == '5' || pick.equals("exit game")) {
             gameRunning = false;
         } else {
             System.out.println("Invalid choice!");
@@ -94,32 +86,75 @@ public class Main {
     // add story to the game
     public static void story(int chapter) {
         if (chapter == 1) {
-            System.out.println("You wake up in a dark room that smells like medicine. There is a white door in front of you and a\n" +
-                    "black door to your back. You reach into your pockets and notice that everything is gone. Your keys, your\n" +
-                    "phone, everything. All you have left are some gray sweatpants, a gray t-shirt with a rocket on it, and a\n" +
-                    "leather backpack that you don't remember owning. The room is empty except for one piece of furniture in\n" +
-                    "the room, a painting.");
-            System.out.println("What do you want to do?");
+            System.out.println("You wake up in a dark room that smells like medicine. There is a white door in front " +
+                    "of you and a black door to your back. You reach into your pockets and notice that everything is " +
+                    "gone. Your keys, your phone, everything. All you have left are some gray sweatpants, a gray " +
+                    "t-shirt with a rocket on it, and a leather backpack that you don't remember owning. The room is " +
+                    "empty except for one piece of furniture in the room, a painting.");
+            System.out.println("\nWhat would you like to do?");
             System.out.println("1. Open your backpack");
             System.out.println("2. Open the white door");
             System.out.println("3. Open the black door");
             System.out.println("4. Cry");
-            int choice = takeInputInt();
-            if (choice == 1) {
-                System.out.println("1. Open your backpack");
-            } else if (choice == 2) {
-                System.out.println("2. Open the white door");
-            } else if (choice == 3) {
-                System.out.println("3. Open the black door");
-            } else if (choice == 4) {
-                System.out.println("4. Cry");
+            char choice = takeInputChar();
+            if (choice == '1') {
+                System.out.println("You open your backpack.");
+                gameMenu("inventory");
+            } else if (choice == '2') {
+                System.out.println("You open the white door");
+            } else if (choice == '3') {
+                System.out.println("You open the black door");
+            } else if (choice == '4') {
+                boolean wait = false;
+                System.out.println("Crying won't help you get out of this place.");
+                System.out.println("Go back to the menu? (y/n)");
+                while (!wait) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (takeInputChar() == 'y') {
+                        wait = true;
+                    } else if (takeInputChar() == 'n') {
+                        System.out.println("Don't say no.");
+                    } else {
+                        System.out.println("Invalid choice!");
+                    }
+                }
+            } else if (choice == 't') {
+                gameMenuOpen = true;
             } else {
-                System.out.println("Type a valid integer");
+                System.out.println("Invalid choice!");
             }
         } else if (chapter == 2) {
 
         } else if (chapter == 3) {
 
         }
+    }
+
+    // ask for username input
+    public static String askForUsername() {
+        System.out.println("Please enter your username: ");
+        return takeInputString();
+    }
+
+    // take user input string
+    public static String takeInputString() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    //take user input character
+    public static char takeInputChar() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next().charAt(0);
+    }
+
+    //take user input int
+    public static int takeInputInt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
